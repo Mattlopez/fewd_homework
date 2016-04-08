25dca2cde41759d6815b4982be579cb4a5434785
muuze49 <matthewlopez@Matthews-MacBook-Pro.local>
2016-04-07 21:53:09 -0700

    8ball_revisions

--- 06-week/magic_8ball/js/app.js
+++ 06-week/magic_8ball/js/app.js
@@ -1,14 +1,37 @@
   $(document).ready(function() {
 
-    alert("It's working")
-
     var prefix = ["Results unclear", "Signs point to yes!", "Ask again later", "Yes", "No", "Hell No","Maybe"]
 
     function buttonClicked(){
       var randomNumber = Math.floor(Math.random() * prefix.length)
       var result = prefix [randomNumber]
       $('#output').text(result)
-      console.log("result")
+      console.log()
+
+      if(result == "Results unclear"){
+        $('body').attr("class","red")
+      }
+      else if(result == "Yes"){
+        $('body').attr("class","green")
+      }
+      else if(result == "No"){
+        $('body').attr("class","red")
+      }
+      else if(result == "Hell No"){
+        $('body').attr("class","red")
+      }
+      else if(result == "Maybe"){
+        $('body').attr("class","blue")
+      }
+      else if(reslt == "Signs point to yes!"){
+        $('body').attr("class","green")
+      }
+      else if(result == "Ask again later"){
+        $('body').attr("class","black")
+      }
+
+
+
     }
 
     $('#shakeMe').click(buttonClicked)
